8200f5b76fa6e5a960aea339c5534a7c01ab4eb2
unknown <juuanduuke@gmail.com>
2020-04-16 00:57:17 -0500

    funcionalidades de filter para campaign

--- backend/src/Controllers/InscriptionController.php
+++ backend/src/Controllers/InscriptionController.php
@@ -4,8 +4,8 @@
 namespace App\Controllers;
 
 
+use App\Models\Campaign;
 use App\Models\Document;
-use App\Models\Field;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -18,60 +18,22 @@ class InscriptionController extends Controller
         $campaign_id = $request->getParsedBody()['idcampaign'];
         $documentAspirant = $request->getParsedBody()['documentAspirant'];
         try {
+            $campaign = Campaign::findOrFail($campaign_id);
+
+            $fields = collect($campaign->fields->toArray());
+
             if (! Document::where('document', $documentAspirant)->exists()) {
                 Document::create(['document' => $documentAspirant]);
             }
             foreach ($request->getParsedBody()['answers'] AS $k => $v) {
-                $field = Field::findOrFail($k);
-                if ($field->required == 1 and !empty($v['answer'])) {
-                    var_dump("[Requeridos]");
-                    if ($field->typeField == 'multipleField') {
-                       if ($field->type == 'text' and is_string($v['answer'])) {
-                            var_dump("[{$field->typeField}] Value del id: {$k} respuesta: {$v['answer']}");
-                       }
-                       if($field->type == 'number' and is_numeric($v['answer'])) {
-                           var_dump("[{$field->typeField}] Value del id: {$k} respuesta: {$v['answer']}");
-                       }
-                       if($field->type == 'date') {
-                           var_dump("[{$field->typeField}] Value del id: {$k} respuesta: {$v['answer']}");
-                       }
-                   } elseif ($field->typeField == 'selectField') {
-                        var_dump("[{$field->typeField}] value del id {$k} respuesta {$v['answer']}");
-                   } elseif ($field->typeField == 'checkField') {
-                        if (is_array($v['answer'])) {
-                            var_dump("[{$field->typeField}] Value id {$k} ". implode(",", $v['answer']));
-                            continue;
-                        }
-                        var_dump("[{$field->typeField}] value del id {$k} respuesta {$v['answer']}");
-                   } elseif($field->typeField == 'textBox') {
-                        var_dump("[{$field->typeField}] value del id {$k} respuesta {$v['answer']}");
-                   } elseif(in_array($field->typeField, ["countriesField", "departmentsField", "townsField", "neighborhoodField"])) {
-                        var_dump("[{$field->typeField}] value del id {$k} respuesta {$v['answer']}");
-                   }
-                } elseif ($field->required == 0 and !empty($v['answer'])) {
-                    var_dump("[No Requeridos]");
-                    if ($field->typeField == 'multipleField') {
-                        if ($field->type == 'text' and is_string($v['answer'])) {
-                            var_dump("[{$field->typeField}] Value del id: {$k} respuesta: {$v['answer']}");
-                        }
-                        if($field->type == 'number' and is_numeric($v['answer'])) {
-                            var_dump("[{$field->typeField}] Value del id: {$k} respuesta: {$v['answer']}");
-                        }
-                        if($field->type == 'date') {
-                            var_dump("[{$field->typeField}] Value del id: {$k} respuesta: {$v['answer']}");
-                        }
-                    }
-
-                } else {
-                    var_dump("[ERROR] Error for id {$k} vacio");
-                }
+                $field = (object) $fields->where('id', $k)->first();
+                var_dump($v);
+                var_dump(evaluateField($field, $v['answer']));
             }
+            die;
         } catch (\Exception $exception) {
             die("[ERROR] Error for ". $exception->getMessage());
         }
-        die;
-        $response->getBody()->write(json_encode($request->getParsedBody()));
-        return $response->withHeader('Content-Type', 'application/json');
 
     }
 
--- backend/src/helpers.php
+++ backend/src/helpers.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Models\Field;
+use Illuminate\Support\Collection;
 
-function saveField(array $value) : array {
+function saveField(array $value): array
+{
     $parseField = parseField($value);
-    if (! isset($value['idField'])) {
+    if (!isset($value['idField'])) {
         $field = Field::create($parseField);
         $value['idField'] = $field->id;
     } else {
@@ -16,7 +18,7 @@ function saveField(array $value) : array {
     return $value;
 }
 
-function parseField(array $field) : array
+function parseField(array $field): array
 {
     $parseField = $field['configurations'];
     $parseField['typeField'] = $field['typeField'];
@@ -25,3 +27,158 @@ function parseField(array $field) : array
     return $parseField;
 }
 
+function evaluateField(object $field, $value)
+{
+    $message = [];
+    $type = getTypeField($field);
+    if (!is_null($type)) {
+        $valideForType = valideForTypeField($field, $type, $value);
+        if (is_array($valideForType)) {
+            $message = array_merge($message, $valideForType);
+            return $message;
+        }
+        return null;
+    }
+    return $message = [
+        "message" => "El typeField no existe",
+        "field" => $field->id
+    ];
+}
+
+function valideForTypeField(object $field, $type, $value)
+{
+    $messsage = [];
+    foreach ($type as $k => $condition) {
+        if (!is_null($field->{$condition})) {
+            $response = validating($field, $condition, $value);
+            if (is_array($response)) {
+              array_push($messsage, $response);
+            }
+        }
+    }
+    return count($messsage) > 0 ? $messsage : null;
+}
+
+function validating(object $field, string $condition, $value): ?array
+{
+    $message = [];
+    switch ($condition) {
+        case "min" :
+            if ($field->min > strlen($value)) {
+                $message = [
+                    "message" => "Es menor que el meinimo requerido",
+                    "field" => $field->id
+                ];
+            }
+            break;
+        case "max" :
+            if ($field->max < strlen($value)) {
+                $message = [
+                    "message" => "Es mayor que el maximo requerido",
+                    "field" => $field->id
+                ];
+            }
+            break;
+        case "defaultValue" :
+            if ($field->defaultValue !== $value and !empty($field->defaultValue)) {
+                $message = [
+                    "message" => "El valor es diferente que el por defecto",
+                    "field" => $field->id
+                ];
+            }
+            break;
+        case "options" :
+            $htmlValues = array_column(json_decode($field->options), 'htmlValue');
+            if (is_array($value)) {
+                $value = explode(",", $value);
+            }
+            if (!in_array($value, $htmlValues)) {
+                $message = [
+                    "message" => "El valor es diferente que las opciones del item",
+                    "field" => $field->id
+                ];
+            }
+            break;
+        case "type" :
+            if (!getEvaluate($field->type, $value)) {
+                $message = [
+                    "message" => "El valor es diferente al tipo solicitado",
+                    "field" => $field->id
+                ];
+            }
+            break;
+        case "required" :
+            if ($field->required and empty($value)) {
+                $message = [
+                    "message" => "El valor es requerido",
+                    "field" => $field->id
+                ];
+            }
+            break;
+        default : break;
+    }
+    return count($message) > 0 ? $message : null;
+}
+
+function getEvaluate(string $evaluate, $value): bool
+{
+    $callables = [
+        "number" => "is_numeric",
+        "text" => "is_string",
+        "date" => "is_date",
+    ];
+    if($evaluate == "number" and $value < 0) {
+        return false;
+    }
+    $callable = $callables[$evaluate];
+    return $callable($value);
+}
+
+function is_date($date, $format = 'Y-m-d H:i:s'): bool
+{
+    $d = DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) == $date;
+}
+
+function getTypeField(object $field): ?array
+{
+    $typeField = [
+        "selectField" => [
+            "options", "required"
+        ],
+        "checkField" => [
+            "options", "required"
+        ],
+        "radioField" => [
+            "options", "required"
+        ],
+        "multipleField" => [
+            "type",
+            "defaultValue",
+            "min",
+            "max",
+            "required"
+        ],
+        "countriesField" => [
+            "required"
+        ],
+        "departmentsField" => [
+            "required"
+        ],
+        "townsField" => [
+            "required"
+        ],
+        "neighborhoodField" => [
+            "required"
+        ]
+    ];
+
+    return $typeField[$field->typeField] ?? null;
+}
+
+if (!function_exists('collect')) {
+    function collect($items)
+    {
+        return new Collection($items);
+    }
+}
